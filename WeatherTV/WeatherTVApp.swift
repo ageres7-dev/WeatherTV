f7099baf6a6c4c7f3b133acc28c0901f1b1e5f85
ageres7-dev <66694244+ageres7-dev@users.noreply.github.com>
2021-03-08 15:18:23 +0500

    Initial Commit

---
+++ WeatherTV/WeatherTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTVApp.swift
+//  WeatherTV
+//
+//  Created by Сергей on 08.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
